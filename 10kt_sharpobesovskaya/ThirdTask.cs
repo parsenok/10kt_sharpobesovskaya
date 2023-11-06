a31ea8f5fbd0bf1ee0b5e46ffde125a3a17fabcb
Максим Симан <maksimsiman832@gmail.com>
2023-11-06 22:49:08 +0300

    3 таск

---
+++ 10kt_sharpobesovskaya/ThirdTask.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10kt_sharpobesovskaya
+{
+    interface IComparer<T> where T : struct
+    {
+        int Compare(T x, T y);
+    }
+    struct ComplexNumber : IComparer<ComplexNumber>
+    {
+        public int Compare(ComplexNumber x, ComplexNumber y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    struct RationalNumber : IComparer<RationalNumber>
+    {
+        public int Compare(RationalNumber x, RationalNumber y)
+        {
+            if (x is RationalNumber)
+            {
+                Console.WriteLine("объекты равны");
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine("объекты не равны");
+                return 1;
+            }
+            if (y is RationalNumber)
+            {
+                Console.WriteLine("объекты равны");
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine("объекты не равны");
+                return 1;
+            }
+        }
+    }
+}
